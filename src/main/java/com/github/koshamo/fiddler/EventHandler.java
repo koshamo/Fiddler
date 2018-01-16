fc0a0a4f2c5d9f4919096ef2d1b60de2676c35ff
koshamo <jochen.rassler@gmail.com>
2018-01-16 07:27:26 +0100

    UTF8 source code encoding

--- src/main/java/com/github/koshamo/fiddler/EventHandler.java
+++ src/main/java/com/github/koshamo/fiddler/EventHandler.java
@@ -1,5 +1,5 @@
 /*
- * Copyright [2017] [Dr. Jochen Raﬂler]
+ * Copyright [2017] [Dr. Jochen Ra√üler]
  * 
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
